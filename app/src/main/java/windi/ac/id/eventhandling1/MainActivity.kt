4a40c0f5258b71f9d3b0ee449ba7f78eb068cc64
AndroidAPPMobile <windiartikah25@gmail.com>
2022-04-22 10:37:59 +0700

    EventHandling1

---
+++ app/src/main/java/windi/ac/id/eventhandling1/MainActivity.kt
@@ -0,0 +1,28 @@
+package windi.ac.id.eventhandling1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext, "Hello World",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        btnDisplayMessage.setOnLongClickListener {
+            Snackbar.make(root_layout, "Long click",
+                Snackbar.LENGTH_LONG).show()
+            true
+        }
+    }
+}
